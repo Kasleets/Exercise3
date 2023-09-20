a15167029630ee8ef1c7de6b2edf3fa7a8686ce9
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-09-20 19:42:19 +0200

    Backup

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -13,6 +13,16 @@
 
             personHandler.SetAge(Stefan, 33);
             Console.WriteLine($"Updated Age: {Stefan.Age}");
+            Console.WriteLine($"Created a person: {Stefan.FName} {Stefan.LName} of Age: {Stefan.Age} of Height: {Stefan.Height} of Weight: {Stefan.Weight}\n");
+
+
+            Person Monika = personHandler.CreatePerson(27, "Monika", "Touric", 172.7, 52);
+            personHandler.SetWeight(Monika, 57.4);
+            Console.WriteLine($"Created a person: {Monika.FName} {Monika.LName} of Age: {Monika.Age} of Height: {Monika.Height} of Weight: {Monika.Weight}");
+            Console.WriteLine($"Updated Weight: {Monika.Weight}");
+
+
+
 
             //try
             //{
